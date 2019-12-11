3d6513456a22f52b7253380e1d8c43c1f3101f7a
scrotify <scrotifyteam@gmail.com>
2019-12-11 12:45:00 +0530

    added entity

---
+++ src/main/java/com/scrotify/medicalclaim/entity/ClaimRequest.java
@@ -0,0 +1,30 @@
+package com.scrotify.medicalclaim.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="claimrequest")
+public class ClaimRequest {
+	@Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long claimRequestId;
+	private Long claimId;
+	private String claimStatus;
+	@Column(name="assigned")
+	private Long approverId;
+	private String reason;
+}
--- src/main/java/com/scrotify/medicalclaim/util/MedicalClaimConstants.java
+++ src/main/java/com/scrotify/medicalclaim/util/MedicalClaimConstants.java
@@ -1,5 +1,7 @@
 package com.scrotify.medicalclaim.util;
 
 public class MedicalClaimConstants {
-
+	
+public final static String POLICY_NOT_FOUNd="";
+public final static String LOGIN_FAILURE="";
 }
